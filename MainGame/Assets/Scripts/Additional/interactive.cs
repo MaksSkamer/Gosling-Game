856cdc16686188c7293ed1f7202495dee0b3bdae
X1ney <andreyka150104@gmail.com>
2023-04-07 17:47:55 +0700

    scripts
    добавил interactive
    для взаимодействия с объектами

--- MainGame/Assets/Scripts/Additional/interactive.cs
+++ MainGame/Assets/Scripts/Additional/interactive.cs
@@ -4,5 +4,37 @@ using UnityEngine;
 
 public class Interactable : Collectable
 {
-    public string objectname;
+    //записываем в ручную название объекта. По этой переменной будут вызываться нужные методы
+    public string objectName;
+    public GameObject item;//сам предмет. Для саписи в инвентарь
+    
+    
+
+    protected override void OnCollectpl()
+    {
+        switch (objectName)
+        {
+            case "door":
+
+                break;
+
+            case "takingitem":
+                TakeItem(item);
+                break;
+            
+            case " ":
+
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    protected void TakeItem(GameObject item)
+    {
+        Debug.Log("подбирается");
+        Destroy(gameObject);
+    }
+
 }

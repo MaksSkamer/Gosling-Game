167e7de4d42e273550041b6821658695728c4053
MaksSkamer <bushmanov.03mak@mail.ru>
2023-04-19 17:39:00 +0700

    Игра(0.35%)
    Добавлено игровое меню паузы
    В будущем визуальная составляющая будет доработана

---
+++ MainGame/Assets/Scripts/UI/Pause_menu.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Pause_menu : MonoBehaviour
+{
+   
+    public static bool GameIsPaused = false;
+
+    public GameObject pauseMenu;
+
+    public Image osu;
+
+    int count = 0;
+
+    void Update()
+    {
+       if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(GameIsPaused) 
+                Resume();
+            else
+                Pause();
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+    void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused= true;
+    }
+
+    public void LoadMenu()
+    {
+        Debug.Log("Load");
+        Time.timeScale= 1f;
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+
+    public void OsuFuck()
+    {
+        count++;
+        Debug.Log(count);
+        if (count >= 10)
+        {
+            osu.color = Color.white;
+            Application.OpenURL("https://osu.ppy.sh/home");
+        }
+        else
+            osu.color = Color.clear;
+    }
+}

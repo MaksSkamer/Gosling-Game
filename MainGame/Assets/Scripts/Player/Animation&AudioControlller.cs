110a5ea8e6c2039b3c4d0b6a3c632572899bd807
MaksSkamer <bushmanov.03mak@mail.ru>
2023-05-13 17:11:58 +0700

    Игра(0,90%)
    Обновлён скин главного персонажа и соответственно анимация бега
    Начата попытка сделать нормальную анимацию оружия (статика, бег и атака)
    +0,5 за отсутствие юнит-тестов

--- MainGame/Assets/Scripts/Player/Animation&AudioControlller.cs
+++ MainGame/Assets/Scripts/Player/Animation&AudioControlller.cs
@@ -10,9 +10,11 @@ public class AnimationAudioControlller : MonoBehaviour
     protected AudioSource a;
     public AudioClip[] sound;
     private Animator animShad;
+    private Animator weapAnim;
     void Start()
     {
         animShad = transform.GetChild(1).GetComponent<Animator>();
+        weapAnim = transform.GetChild(0).GetComponent<Animator>();
         anim = GetComponent<Animator>();
         a = GetComponent<AudioSource>();
     }
@@ -23,7 +25,8 @@ public class AnimationAudioControlller : MonoBehaviour
         if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S))
         {
             a.volume = 0.8f;
-            anim.Play("Player_walk");
+            anim.Play("Player_walk_cl");
+            weapAnim.Play("Weapon_motion");
             animShad.Play("Player_shadow_motion");
             GameManager.instance.Sound(a, sound, 0);
         }
@@ -31,6 +34,7 @@ public class AnimationAudioControlller : MonoBehaviour
         {
             animShad.Play("Player_shadow_idle");
             anim.Play("Player_idle");
+            weapAnim.Play("weapon_idle");
         }
 
         if(Input.GetKeyDown(KeyCode.LeftShift) && (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S)) && !dashCD)
